a29daee4d31a1a702a703683829b9a4f1cd0ce75
Anay Joshi <aj07msd@outlook.com>
2026-06-02 22:09:58 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,10 +21,9 @@ origins = [
     "https://fitness-tracker-vb2x.vercel.app",
 ]
 
-# CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
